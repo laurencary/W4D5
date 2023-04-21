945419602623c560268376f4f91729c2a29c5dc6
Shanna Le <shannatle@gmail.com>
2023-04-21 16:37:18 -0700

    Finish 3rd and 4th problems

--- rspec_demo/lib/array.rb
+++ rspec_demo/lib/array.rb
@@ -1,3 +1,5 @@
+
+
 def uniq(array)
     results = []
     array.each do |ele|
@@ -22,3 +24,30 @@ def two_sum(array)
     pairs
 
 end
+
+def my_transpose(array)
+    results = []
+    (0...array.length).each do |row|
+        new_row = [] 
+        (0...array.length).each do |col|
+            new_row << array[col][row]
+        end 
+        results << new_row 
+    end 
+    results 
+end
+
+def stock_picker(prices)
+    max_difference = 0 
+    pairs = []
+    (0...prices.length-1).each do |i|
+        if prices[i+1] - prices[i] > max_difference
+            max_difference = prices[i+1] - prices[i]
+            pairs = [i,i+1]
+        end 
+    end 
+    pairs 
+
+end 
+
+# stock_picker([1,3,4,0,6,8,3])
--- rspec_demo/spec/array_spec.rb
+++ rspec_demo/spec/array_spec.rb
@@ -18,3 +18,47 @@ describe "two_sum" do
     end
 end
 
+describe "my_transpose" do 
+    let(:array1) {[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+      ]}
+    let(:result1) {[
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+      ]}
+    let(:array2) {[
+        [0,1], 
+        [2,3]
+      ]}
+    let(:result2) {[
+        [0,2],
+        [1,3]
+    ]}
+
+    it "will transpose a 3x3 array" do 
+        expect(my_transpose(array1)).to eq(result1)
+    end 
+
+    it "will transpose a 2x2 array" do 
+        expect(my_transpose(array2)).to eq(result2)
+    end 
+
+end 
+
+# [1,3,4,0,6,8,3]
+describe "stock_picker" do 
+    let(:prices) {[1,3,4,0,6,8,3]}
+
+    it "returns array with 2 numbers" do 
+        expect(stock_picker(prices).is_a?(Array)).to be true
+        expect(stock_picker(prices).length).to eq(2)
+    end 
+
+    it "returns a pair of indicies with the greatest difference" do 
+        expect(stock_picker(prices)).to eq([3,4])
+    end 
+
+end 
